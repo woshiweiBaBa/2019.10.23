cbb31e2e23b8121efdee69bd79c70a7d63df79a7
zs <zs@bjpowernode.com>
2019-10-23 11:57:57 +0800

    22334455

---
+++ src/com/User.java
@@ -0,0 +1,16 @@
+package com;/**
+ * className:User
+ * package:com
+ * Description:
+ *
+ * @date:2019/10/23 11:54
+ * @author:LiuJingWei
+ */
+
+/**
+ * author:刘经纬
+ * 2019/10/23
+ */
+public class User {
+    private String name;
+}
